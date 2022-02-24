7faba7698f3c4f7cb47cc01bd5f6be9e44c4a0e5
samikb1 <58870163+samikb1@users.noreply.github.com>
2022-02-24 16:21:53 +0530

    Update dbemployeechangelog.sql

--- dbscripts/deploy/dbemployeechangelog.sql
+++ dbscripts/deploy/dbemployeechangelog.sql
@@ -1,7 +1,7 @@
 use database LIQUIBASE_DB;
 use schema LIQUIBASE;
 
-CREATE OR REPLACE TABLE EMPLOYEE
+CREATE OR REPLACE TABLE EMPLOYEE1
 (
 emp_id integer not null,
 emp_name varchar(50) not null);
